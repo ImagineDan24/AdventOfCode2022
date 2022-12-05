b01accb830736d2fda409217cf591f1566d64af3
Daniel-Podsadowski <35496162+Daniel-Podsadowski@users.noreply.github.com>
2022-12-04 22:03:26 -0500

    Setup for git and advent of code stuff

---
+++ AdventOfCode2022/Day/Day1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2022.Day
+{
+    public static class Day1
+    {
+        public static void Main()
+        {
+            var fileInput = @"..\..\..\Data\Day1.txt";
+            var lines = File.ReadAllLines(fileInput);
+
+            Console.WriteLine("Commencing Day 1, Part A...");
+
+
+
+
+
+
+        }
+    }
+}
