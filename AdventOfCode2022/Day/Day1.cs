6859ddad35c6ad62e2e4b91ab6df75d4422c1d47
Daniel-Podsadowski <35496162+Daniel-Podsadowski@users.noreply.github.com>
2022-12-04 22:23:25 -0500

    Finished Day 1, Part 2. Also added better separation between Parts 1 and 2

--- AdventOfCode2022/Day/Day1.cs
+++ AdventOfCode2022/Day/Day1.cs
@@ -13,12 +13,18 @@ namespace AdventOfCode2022.Day
             var fileInput = @"..\..\..\Data\Day1.txt";
             var lines = File.ReadAllLines(fileInput);
 
+            Part1(lines);
+            Part2(lines);
+        }
+
+        public static void Part1(String[]? lines)
+        {
             Console.WriteLine("Commencing Day 1, Part 1...");
 
             int highestCals = 0;
             int currentCalSum = 0;
 
-            for (int i=0; i<lines.Length; i++)
+            for (int i = 0; i < lines.Length; i++)
             {
                 if (lines[i] == "")
                 {
@@ -34,7 +40,48 @@ namespace AdventOfCode2022.Day
                 }
             }
             Console.WriteLine("Answer: " + highestCals);
+        }
 
+        public static void Part2(String[]? lines)
+        {
+            Console.WriteLine("Commencing Day 1, Part 2...");
+
+            int highestCals = 0;
+            int secondHighestCals = 0;
+            int thirdHighestCals = 0;
+            int currentCalSum = 0;
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                if (lines[i] == "")
+                {
+                    if (currentCalSum > highestCals)
+                    {
+                        thirdHighestCals = secondHighestCals;
+                        secondHighestCals = highestCals;
+                        highestCals = currentCalSum;
+
+                    }
+                    else if (currentCalSum > secondHighestCals && currentCalSum < highestCals)
+                    {
+                        thirdHighestCals = secondHighestCals;
+                        secondHighestCals = currentCalSum;
+                    }
+                    else if (currentCalSum > thirdHighestCals && currentCalSum < secondHighestCals)
+                    {
+                        thirdHighestCals = currentCalSum;
+                    }
+                    currentCalSum = 0;
+                }
+                else
+                {
+                    currentCalSum += int.Parse(lines[i]);
+                }
+            }
+
+            var answer = highestCals + secondHighestCals + thirdHighestCals;
+
+            Console.WriteLine("Answer: " + answer);
         }
     }
 }
