0d08adbe4da8e6b35d1883801ef118b1abda7721
daniel9832 <daniel9832@smtp.gc.ca>
2022-12-05 11:28:42 -0500

    Finished Day4

--- AdventOfCode2022/Day/Day4.cs
+++ AdventOfCode2022/Day/Day4.cs
@@ -21,14 +21,38 @@ namespace AdventOfCode2022.Day
         {
             Console.WriteLine("Commencing Day 4, Part 1...");
 
-            
+            int total = 0;
+
+            for (int i=0; i < lines.Length; i++)
+            {
+                var x1 = Int32.Parse(lines[i].Split(',')[0].Split('-')[0]);
+                var x2 = Int32.Parse(lines[i].Split(',')[0].Split('-')[1]);
+                var y1 = Int32.Parse(lines[i].Split(',')[1].Split('-')[0]);
+                var y2 = Int32.Parse(lines[i].Split(',')[1].Split('-')[1]);
+
+                if ((x1 <= y1 && x2 >= y2) || (y1 <= x1 && y2 >= x2)) { total++; }
+            }
+
+            Console.WriteLine("Answer; " + total);
         }
 
         public static void Part2(String[] lines)
         {
             Console.WriteLine("Commencing Day 4, Part 2...");
 
-            
+            int total = 0;
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                var x1 = Int32.Parse(lines[i].Split(',')[0].Split('-')[0]);
+                var x2 = Int32.Parse(lines[i].Split(',')[0].Split('-')[1]);
+                var y1 = Int32.Parse(lines[i].Split(',')[1].Split('-')[0]);
+                var y2 = Int32.Parse(lines[i].Split(',')[1].Split('-')[1]);
+                
+                if ((x1 <= y1 && y1 <= x2) || (y1 <= x1 && x1 <= y2)) { total++; }
+            }
+
+            Console.WriteLine("Answer; " + total);
         }
     }
 }
