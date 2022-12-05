7352145f1c4fa1a745ac43c1e497f5be33c6ae70
Daniel-Podsadowski <35496162+Daniel-Podsadowski@users.noreply.github.com>
2022-12-04 22:28:30 -0500

    Data and function setup for Day 3 & 4

---
+++ AdventOfCode2022/Day/Day3.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2022.Day
+{
+    public static class Day4
+    {
+        public static void Main()
+        {
+            var fileInput = @"..\..\..\Data\Day4.txt";
+            var lines = File.ReadAllLines(fileInput);
+
+            Part1(lines);
+            Part2(lines);
+        }
+
+        public static void Part1(String[]? lines)
+        {
+            Console.WriteLine("Commencing Day 4, Part 1...");
+
+            
+        }
+
+        public static void Part2(String[]? lines)
+        {
+            Console.WriteLine("Commencing Day 4, Part 2...");
+
+            
+        }
+    }
+}
---
+++ AdventOfCode2022/Day/Day4.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2022.Day
+{
+    public static class Day3
+    {
+        public static void Main()
+        {
+            var fileInput = @"..\..\..\Data\Day3.txt";
+            var lines = File.ReadAllLines(fileInput);
+
+            Part1(lines);
+            Part2(lines);
+        }
+
+        public static void Part1(String[]? lines)
+        {
+            Console.WriteLine("Commencing Day 3, Part 1...");
+
+            
+        }
+
+        public static void Part2(String[]? lines)
+        {
+            Console.WriteLine("Commencing Day 3, Part 2...");
+
+            
+        }
+    }
+}
