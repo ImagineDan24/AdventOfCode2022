466ddf6fe4232864054c8c6673b024952aa9fed0
Daniel-Podsadowski <35496162+Daniel-Podsadowski@users.noreply.github.com>
2022-12-04 22:25:40 -0500

    Added Day2 data and setup

---
+++ AdventOfCode2022/Day/Day2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2022.Day
+{
+    public static class Day2
+    {
+        public static void Main()
+        {
+            var fileInput = @"..\..\..\Data\Day2.txt";
+            var lines = File.ReadAllLines(fileInput);
+
+            Part1(lines);
+            Part2(lines);
+        }
+
+        public static void Part1(String[]? lines)
+        {
+            Console.WriteLine("Commencing Day 2, Part 1...");
+
+            
+        }
+
+        public static void Part2(String[]? lines)
+        {
+            Console.WriteLine("Commencing Day 2, Part 2...");
+
+            
+        }
+    }
+}
