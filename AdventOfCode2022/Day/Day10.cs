6aca75d7d1537d7b7d190ba37d84c254d8369f26
Daniel-Podsadowski <35496162+Daniel-Podsadowski@users.noreply.github.com>
2022-12-10 16:10:19 -0500

    Completed Day 10 and setup Day 11

--- AdventOfCode2022/Day/Day10.cs
+++ AdventOfCode2022/Day/Day10.cs
@@ -4,7 +4,7 @@
     {
         public static void Main()
         {
-            var fileInput = @"..\..\..\Data\Test.txt";
+            var fileInput = @"..\..\..\Data\Day10.txt";
             var lines = File.ReadAllLines(fileInput);
 
             if (lines != null)
@@ -14,20 +14,72 @@
             }
         }
 
+        public static List<string> toCommandList(String[] lines)
+        {
+            var list = new List<string>();
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                if (lines[i] == "noop")
+                {
+                    list.Add(lines[i]);
+                }
+                else
+                {
+                    list.Add("noop");
+                    list.Add(lines[i]);
+                }
+            }
+
+            return list;
+        }
+
         public static void Part1(String[] lines)
         {
             Console.WriteLine("Commencing Day 10, Part 1...");
 
+            var list = toCommandList(lines);
 
-            
+            var register = 1;
+            var sum = 0;
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (i == 19 || i == 59 || i == 99 || i == 139 || i == 179 || i == 219)
+                {
+                    sum += (i + 1) * register;
+                }
+
+                if (list[i] != "noop")
+                {
+                    register += Int32.Parse(list[i].Split(" ")[1]);
+                }
+            }
+
+            Console.WriteLine("Answer: " + sum);
         }
 
         public static void Part2(String[] lines)
         {
             Console.WriteLine("Commencing Day 10, Part 2...");
+            Console.WriteLine("Answer: ");
 
-            
+            var list = toCommandList(lines);
 
+            var register = 1;
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (i%40 == register || i%40 == register - 1 || i%40 == register + 1) { Console.Write("#"); }
+                else { Console.Write("."); }
+                
+                if (list[i] != "noop")
+                {
+                    register += Int32.Parse(list[i].Split(" ")[1]);
+                }
+
+                if (i == 39 || i == 79 || i == 119 || i == 159 || i == 199 || i == 239) { Console.WriteLine(); }
+            }
         }
     }
 }
---
+++ AdventOfCode2022/Day/Day11.cs
@@ -0,0 +1,33 @@
+﻿namespace AdventOfCode2022.Day
+{
+    public static class Day11
+    {
+        public static void Main()
+        {
+            var fileInput = @"..\..\..\Data\Test.txt";
+            var lines = File.ReadAllLines(fileInput);
+
+            if (lines != null)
+            {
+                Part1(lines);
+                Part2(lines);
+            }
+        }
+
+        public static void Part1(String[] lines)
+        {
+            Console.WriteLine("Commencing Day 11, Part 1...");
+
+            
+
+        }
+
+        public static void Part2(String[] lines)
+        {
+            Console.WriteLine("Commencing Day 11, Part 2...");
+            
+
+
+        }
+    }
+}
